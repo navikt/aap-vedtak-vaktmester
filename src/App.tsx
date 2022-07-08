c7e277808542998ee61fd7cf1a63b9faf3b82262
Øivind Stensrud <oivind.stensrud@nav.no>
2022-07-08 08:44:30 +0200

    Send spørring

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import "@navikt/ds-css";
+import { Topics } from "./Topics";
+import { Heading } from "@navikt/ds-react";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div>AAP Vedtak Vaktmester</div>
-  )
+    <div className={"appContainer"}>
+      <Heading size={"xlarge"} level={"1"}>
+        AAP Vedtak Vaktmester
+      </Heading>
+      <QueryClientProvider client={queryClient}>
+        <Topics />
+      </QueryClientProvider>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,6 @@
 import { rest } from "msw";
 export const handlers = [
   rest.get("/aap-vaktmester/api/topics", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(["søkere.api.v1", "søknader.api.v1"]), ctx.delay(600));
+    return res(ctx.status(200), ctx.json(["søkere.api.v1", "søknader.api.v1"]), ctx.delay(70000));
   }),
 ];
