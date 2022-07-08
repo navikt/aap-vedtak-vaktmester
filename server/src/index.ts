54a400b7f4a6b63d7a9427739966466359e0354a
Øivind Stensrud <oivind.stensrud@nav.no>
2022-07-08 07:58:53 +0200

    Config for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,42 @@ import express from "express";
 import {LogInfo} from "./logger";
 import path from "path";
 import config from "./config";
+import client from "prom-client";
 
 const PORT = process.env.PORT || 3000;
 const BUILD_PATH = path.join(__dirname, "../dist");
 const server = express();
 
 const startServer = () => {
+
+  // Create a Registry which registers the metrics
+  const register = new client.Registry();
+  // Add a default label which is added to all metrics
+  register.setDefaultLabels({
+    app: "aap-vaktmester",
+  }); // Enable the collection of default metrics
+  client.collectDefaultMetrics({ register });
+
+  server.use(express.static(BUILD_PATH));
+  server.use(express.json());
+
+  // health checks
+  server.get(
+    [
+      `${config.BASE_PATH}/internal/isAlive`,
+      `${config.BASE_PATH}/internal/isReady`,
+    ],
+    (req: any, res: any) => res.sendStatus(200)
+  );
+
+  server.get(
+    `${config.BASE_PATH}/internal/metrics`,
+    async (req: any, res: any) => {
+      res.setHeader("Content-Type", register.contentType);
+      res.end(await register.metrics());
+    }
+  );
+
   // Render app
   server.get(`${config.BASE_PATH}/*`, (req: any, res: any) =>
     res.sendFile(path.join(BUILD_PATH, "index.html"))
