130167e95d3981295cc6f42a417f3330a049f37c
Øivind Stensrud <oivind.stensrud@nav.no>
2022-07-08 10:04:58 +0200

    Feil property-navn

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,12 +1,12 @@
-const APP_URL = process.env.APP_URL || 'localhost:3000'
-const DEVTOOLS_API_URL = process.env.OPPGAVESTYRING_API_URL || 'localhost:5000'
-const DEVTOOLS_API_AUDIENCE = process.env.DEVTOOLS_API_AUDIENCE || 'localhost:devtools'
-const DEVTOOLS_API_SCOPE = process.env.DEVTOOLS_API_SCOPE || 'api://localhost.devtools/.default'
+const APP_URL = process.env.APP_URL || "localhost:3000";
+const DEVTOOLS_API_URL = process.env.DEVTOOLS_API_URL || "localhost:5000";
+const DEVTOOLS_API_AUDIENCE = process.env.DEVTOOLS_API_AUDIENCE || "localhost:devtools";
+const DEVTOOLS_API_SCOPE = process.env.DEVTOOLS_API_SCOPE || "api://localhost.devtools/.default";
 /* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
-  BASE_PATH: '/aap-vaktmester',
+  BASE_PATH: "/aap-vaktmester",
   APP_URL,
   DEVTOOLS_API_URL,
   DEVTOOLS_API_AUDIENCE,
-  DEVTOOLS_API_SCOPE
+  DEVTOOLS_API_SCOPE,
 };
