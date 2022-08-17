621591675c94b69a00e9b4c291dd51cac297c78f
Øivind Stensrud <oivind.stensrud@nav.no>
2022-08-17 15:27:10 +0200

    cors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@ const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
   output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "https://aap-devtools.dev.intern.nav.no/" },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
