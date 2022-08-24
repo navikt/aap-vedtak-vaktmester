9d050a2fe9ad686d7685eeee1a44d0d28139f2d4
Øivind Stensrud <oivind.stensrud@nav.no>
2022-08-24 16:26:35 +0200

    Legger inn litt logging

--- pages/api/topics/index.ts
+++ pages/api/topics/index.ts
@@ -6,14 +6,17 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
   if (req.method === "GET") {
     try {
+      console.log("Topic-spørring");
       const result = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/topics`);
       if (result.ok) {
         const data = await result.json();
         return { ok: result.ok, data };
       } else {
+        console.error(`Topic-spørring feilet. HttpStatus: ${res.status}. Message: ${res.statusMessage}`);
         return { ok: result.ok, error: result.statusText };
       }
     } catch (e) {
+      console.error(`Topic-spørring feilet med en uhåndtert feil: ${e}`);
       return { error: `useFetchPOST: ${e}` };
     }
   }
