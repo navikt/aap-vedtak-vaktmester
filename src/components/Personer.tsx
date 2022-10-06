e22851795405a0f2c2b94193cd2382e7fca1feb1
Richard Borge <richard.borge@nav.no>
2022-10-06 13:19:44 +0200

    Knapp for å sende søknad.
    Co-authored-by: Anders Østby <anders.ostby@nav.no>
    Co-authored-by: Robin Tordly <robin.tordly@nav.no>
    Co-authored-by: Øivind Stensrud <oivind.stensrud@nav.no>
    Co-authored-by: Thomas Rognes <thomas.sebastian.rognes@nav.no>

--- src/components/Personer.tsx
+++ src/components/Personer.tsx
@@ -1,5 +1,5 @@
 import useSWR from "swr";
-import {Loader, Table} from "@navikt/ds-react";
+import {Button, Loader, Table} from "@navikt/ds-react";
 import {DollyResponse} from "../types/DollyResponse";
 
 const Personrad = ({data}: {data: DollyResponse}) => {
@@ -10,11 +10,26 @@ const Personrad = ({data}: {data: DollyResponse}) => {
         <Table.DataCell>{data.fødselsnummer}</Table.DataCell>
         <Table.DataCell>{data.navn}</Table.DataCell>
         <Table.DataCell>{data.fødselsdato}</Table.DataCell>
+        <Table.DataCell><Button variant={"primary"} onClick={() => sendSøknad(data.fødselsnummer, data.fødselsdato)}>Send søknad</Button> </Table.DataCell>
       </Table.ExpandableRow>
     </>
   )
 }
 
+const sendSøknad = (fnr: string, fdato: string) => {
+  const postBody = JSON.stringify({ fødselsdato: fdato });
+  // @ts-ignore
+  fetch(`/api/soeknad/${fnr}`, {
+    method: "POST",
+    body: postBody,
+  }).then((res) => {
+    if (res.ok) {
+      alert("Sendt søknad")
+    } else {
+      alert(res.statusText)
+    }
+  });
+}
 
 
 const Personer = () => {
@@ -37,6 +52,7 @@ const Personer = () => {
           <Table.HeaderCell>Fødselsnummer</Table.HeaderCell>
           <Table.HeaderCell>Navn</Table.HeaderCell>
           <Table.HeaderCell>Fødselsdato</Table.HeaderCell>
+          <Table.HeaderCell />
         </Table.Row>
       </Table.Header>
       <Table.Body>
