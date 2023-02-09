c87eafada01aa78544266defc287016fa2d861fe
Øivind Stensrud <oivind.stensrud@nav.no>
2023-02-09 08:43:27 +0100

    Litt dekorasjon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,6 +5,9 @@ import { Header } from "@navikt/ds-react-internal";
 import { useRouter } from "next/router";
 import { Caseworker, People, Search } from "@navikt/ds-icons";
 
+import styles from './Layout.module.css';
+
+const NaisBorder = () => (<div className={styles.naisBorder} />);
 const Layout = ({ children }: { children: ReactElement }) => {
   const router = useRouter();
   return (
@@ -16,6 +19,7 @@ const Layout = ({ children }: { children: ReactElement }) => {
       <Header>
         <Header.Title href={"/"}>AAP Vedtak Vaktmester</Header.Title>
       </Header>
+      <NaisBorder />
       <Tabs value={router.pathname} size={"small"} onChange={(url) => router.push(url, undefined, { shallow: true })}>
         <Tabs.List>
           <Tabs.Tab value={"/"} label={"Topic-søk"} icon={<Search title={"Søk i topics"} />} />
