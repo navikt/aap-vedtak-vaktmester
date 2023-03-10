d4c51cd2d4356a3378b7cfaab402fcba5a1e7eed
Øivind Stensrud <oivind.stensrud@nav.no>
2023-03-10 11:15:06 +0100

    Fjerner mulighet for å sette sortering
    Skal alltid være LATEST

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,8 +1,8 @@
-import { Topics } from "../Topics";
-import { Button, ErrorMessage, ToggleGroup } from "@navikt/ds-react";
-import { useState } from "react";
-import { TopicResult } from "../topicresult/TopicResult";
-import { format } from "date-fns";
+import {Topics} from "../Topics";
+import {Button, ErrorMessage} from "@navikt/ds-react";
+import {useState} from "react";
+import {TopicResult} from "../topicresult/TopicResult";
+import {format} from "date-fns";
 import useSWR from "swr";
 
 import styles from "./search.module.css";
@@ -10,11 +10,10 @@ import styles from "./search.module.css";
 const Search = () => {
   const [valgtTopic, settValgtTopic] = useState<string>("");
   const [visFeilmelding, settVisFeilmelding] = useState<boolean>(false);
-  const [sortering, settSortering] = useState<string>("LATEST");
   const [sistOppdatert, settSistOppdatert] = useState<string>("Never!");
 
   const kanSøke = valgtTopic !== "" && valgtTopic !== undefined;
-  const { data, error, mutate } = useSWR(kanSøke ? `/api/topic/${valgtTopic}/${sortering}` : null, {
+  const { data, error, mutate } = useSWR(kanSøke ? `/api/topic/${valgtTopic}/LATEST` : null, {
     revalidateOnFocus: false,
   });
 
@@ -37,12 +36,6 @@ const Search = () => {
       <div className={styles.søkelinje}>
         <div className={styles.blokk}>
           <Topics velgTopic={velgTopic} />
-        </div>
-        <div className={styles.blokk}>
-          <ToggleGroup onChange={(value) => settSortering(value)} value={sortering} size={"small"}>
-            <ToggleGroup.Item value={"LATEST"}>Nyeste</ToggleGroup.Item>
-            <ToggleGroup.Item value={"EARLIEST"}>Eldste</ToggleGroup.Item>
-          </ToggleGroup>
         </div>
         <div className={styles.blokk}>
           <Button
