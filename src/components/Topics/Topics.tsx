0b6878c23d8b618d689e1b8d5b63f5c32738168d
Øivind Stensrud <oivind.stensrud@nav.no>
2023-03-14 15:51:58 +0100

    Bytt ut dropdown-liste for topics med knapper

---
+++ src/components/Topics/Topics.tsx
@@ -0,0 +1,80 @@
+import { Button, Heading, Loader } from "@navikt/ds-react";
+import { useState } from "react";
+import useSWR from "swr";
+
+import styles from "./Topics.module.css";
+import { ArrowCirclepathIcon } from "@navikt/aksel-icons";
+
+interface TopicButtonProps {
+  topic: string;
+  isSelected: boolean;
+  onClick: Function;
+}
+const TopicButton = ({ topic, isSelected, onClick }: TopicButtonProps) => {
+  const classNames = `${styles.topicButton} ${isSelected && styles.selected}`;
+  return (
+    <button onClick={() => onClick()} className={classNames}>
+      {topic}
+    </button>
+  );
+};
+const TopicButtons = ({ topics, velgTopic }: { topics: string[]; velgTopic: Function }) => {
+  const [selectedTopic, setSelectedTopic] = useState<string | undefined>();
+  const handleChange = (topic: string) => {
+    setSelectedTopic(topic);
+    velgTopic(topic);
+  };
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      {topics.map((topic) => (
+        <TopicButton
+          topic={topic}
+          onClick={() => handleChange(topic)}
+          isSelected={topic === selectedTopic}
+          key={topic}
+        />
+      ))}
+    </div>
+  );
+};
+
+interface TopicsProps {
+  velgTopic: Function;
+}
+
+const Topics = ({ velgTopic }: TopicsProps) => {
+  const { data, error, mutate, isValidating } = useSWR("/api/topics");
+
+  if (error) {
+    console.error(error);
+    return <div>Klarte ikke å hente topics.</div>;
+  }
+
+  return (
+    <section className={styles.topicSearch}>
+      <div>
+        <Heading size={"small"} level={"2"}>
+          Topics
+        </Heading>
+        <Button
+          variant={"tertiary"}
+          onClick={() => mutate()}
+          icon={<ArrowCirclepathIcon />}
+          size={"small"}
+          disabled={isValidating}
+          className={styles.refresh}
+        >
+          Oppdater topic-liste
+        </Button>
+      </div>
+      {isValidating && <Loader title={"Laster topics..."} className={styles.loader} />}
+      {!isValidating && data && (
+        <div className={styles.topicButtons}>
+          <TopicButtons topics={data} velgTopic={velgTopic} />
+        </div>
+      )}
+    </section>
+  );
+};
+
+export { Topics };
--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,8 +1,6 @@
-import {Topics} from "../Topics";
-import {Button, ErrorMessage} from "@navikt/ds-react";
-import {useState} from "react";
-import {TopicResult} from "../topicresult/TopicResult";
-import {format} from "date-fns";
+import { Topics } from "../Topics";
+import { useState } from "react";
+import { TopicResult } from "../topicresult/TopicResult";
 import useSWR from "swr";
 
 import styles from "./search.module.css";
@@ -10,10 +8,9 @@ import styles from "./search.module.css";
 const Search = () => {
   const [valgtTopic, settValgtTopic] = useState<string>("");
   const [visFeilmelding, settVisFeilmelding] = useState<boolean>(false);
-  const [sistOppdatert, settSistOppdatert] = useState<string>("Never!");
 
   const kanSøke = valgtTopic !== "" && valgtTopic !== undefined;
-  const { data, error, mutate } = useSWR(kanSøke ? `/api/topic/${valgtTopic}/LATEST` : null, {
+  const { data, error } = useSWR(kanSøke ? `/api/topic/${valgtTopic}/LATEST` : null, {
     revalidateOnFocus: false,
   });
 
@@ -22,33 +19,10 @@ const Search = () => {
     visFeilmelding && settVisFeilmelding(false);
   };
 
-  const soekPaaTopic = () => {
-    if (kanSøke) {
-      mutate();
-      settSistOppdatert(format(new Date(), "dd.MM.yyyy HH:mm:ss.SSS"));
-    } else {
-      settVisFeilmelding(true);
-    }
-  };
-
   return (
     <main>
       <div className={styles.søkelinje}>
-        <div className={styles.blokk}>
-          <Topics velgTopic={velgTopic} />
-        </div>
-        <div className={styles.blokk}>
-          <Button
-            variant={"primary"}
-            size={"small"}
-            onClick={() => soekPaaTopic()}
-            loading={!data && !error && kanSøke}
-          >
-            Refresh
-          </Button>
-          {visFeilmelding && <ErrorMessage>Du må velge topic først!</ErrorMessage>}
-        </div>
-        <div className={`${styles.blokk} ${styles.timestamp}`}>Sist oppdatert: {sistOppdatert}</div>
+        <Topics velgTopic={velgTopic} />
       </div>
       <TopicResult searchResult={data} isLoading={!data && !error && kanSøke} error={error} />
     </main>
