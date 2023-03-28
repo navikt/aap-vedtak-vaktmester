de65b2464d141a7e9281281159b03468b6819613
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-03-28 13:07:29 +0200

    Trekker ut SystemMeny.tsx

--- src/components/vaktmesterheader/VaktmesterHeader.tsx
+++ src/components/vaktmesterheader/VaktmesterHeader.tsx
@@ -1,27 +1,12 @@
 import React from "react";
-import { Dropdown, Header } from "@navikt/ds-react-internal";
-import { ExternalLink, System } from "@navikt/ds-icons";
-import { Link } from "@navikt/ds-react";
+import { Header } from "@navikt/ds-react-internal";
+import { SystemMeny } from "./SystemMeny";
 
 const VaktmesterHeader = () => {
   return (
     <Header>
       <Header.Title href={"/"}>AAP Vedtak Vaktmester</Header.Title>
-      <Dropdown>
-        <Header.Button as={Dropdown.Toggle} style={{ marginLeft: "auto" }}>
-          <System style={{ fontSize: "1.5rem" }} title={"Systemer"} />
-        </Header.Button>
-        <Dropdown.Menu>
-          <Dropdown.Menu.GroupedList>
-            <Dropdown.Menu.GroupedList.Heading>Systemer</Dropdown.Menu.GroupedList.Heading>
-            <Dropdown.Menu.GroupedList.Item>
-              <Link href={"https://aap-sink-utforsker.intern.dev.nav.no"} target={"_blank"}>
-                AAP-sink utforsker <ExternalLink />
-              </Link>
-            </Dropdown.Menu.GroupedList.Item>
-          </Dropdown.Menu.GroupedList>
-        </Dropdown.Menu>
-      </Dropdown>
+      <SystemMeny />
     </Header>
   );
 };
