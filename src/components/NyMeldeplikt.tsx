94e20c8b1eaac0ed6fdc959aaeb7e9b1a1c2aa0e
Thomas Rognes <33520517+thomasrognes@users.noreply.github.com>
2023-05-03 13:25:24 +0200

    Refacto
    Co-authored-by: Øivind Stensrud <oivind.stensrud@nav.no>

--- src/components/NyMeldeplikt.tsx
+++ src/components/NyMeldeplikt.tsx
@@ -1,49 +1,21 @@
 import { Alert, Button, Heading, Modal, TextField } from '@navikt/ds-react';
 import { format } from 'date-fns';
-import { RefObject, useRef, useState } from 'react';
+import { useState } from 'react';
 
 import { useDisclosure } from '../hooks/useDisclosure';
-
-const harVerdi = (input: string) => {
-  if (input === undefined || input === null) {
-    return false;
-  }
-  return !(input === '' || input.trim() === '');
-};
+import { getDateArray, validerDato } from '../lib/dates';
 
 const NyMeldeplikt = ({ personident }: { personident: string }) => {
-  const fradatoRef = useRef<HTMLInputElement>(null);
-  const tildatoRef = useRef<HTMLInputElement>(null);
+  const [fraDato, setFraDato] = useState<string>('');
+  const [tilDato, setTilDato] = useState<string>('');
   const [fradatoError, settFraDatoError] = useState<string | undefined>(undefined);
   const [tildatoError, settTilDatoError] = useState<string | undefined>(undefined);
-  const [fradato, settFraDato] = useState<string>('');
-  const [tildato, settTilDato] = useState<string>('');
   const [status, settStatus] = useState<{ status: string; message: string } | undefined>(undefined);
   const [senderData, settSenderData] = useState<boolean>(false);
 
-  const validerDato = (datoRef: RefObject<HTMLInputElement>) => {
-    if (!datoRef.current) {
-      return 'Mangler input-felt';
-    }
-    if (!harVerdi(datoRef.current.value)) {
-      return 'Du må legge inn en dato.';
-    }
-    if (datoRef.current.value.length !== 10) {
-      return 'Dato har ugyldig lengde.';
-    }
-    const datoString = datoRef.current.value;
-    const gyldigDato = Date.parse(datoString);
-    if (isNaN(gyldigDato)) {
-      return `${datoString} er en ugyldig dato`;
-    }
-  };
-
   const validerFelt = () => {
-    if (!fradatoRef.current || !tildatoRef.current) {
-      return false;
-    }
-    const fradatoFeil = validerDato(fradatoRef);
-    const tildatoFeil = validerDato(tildatoRef);
+    const fradatoFeil = validerDato(fraDato);
+    const tildatoFeil = validerDato(tilDato);
     if (fradatoFeil || tildatoFeil) {
       fradatoFeil && settFraDatoError(fradatoFeil);
       tildatoFeil && settTilDatoError(tildatoFeil);
@@ -53,33 +25,12 @@ const NyMeldeplikt = ({ personident }: { personident: string }) => {
     }
   };
 
-  const getDateArray = (start: Date, end: Date) => {
-    const arr = [];
-    const dt = new Date(start);
-
-    while (dt <= end) {
-      arr.push(new Date(dt));
-      dt.setDate(dt.getDate() + 1);
-    }
-
-    return arr;
-  };
-
-  const postData = (event: React.FormEvent) => {
+  const postData = (event) => {
     event.preventDefault();
     if (validerFelt()) {
       settSenderData(true);
 
-      const fom = fradatoRef.current?.value;
-      const tom = tildatoRef.current?.value;
-      if (!fom || !tom) {
-        return;
-      }
-
-      const fraDato: Date = new Date(Date.parse(fom));
-      const tilDato: Date = new Date(Date.parse(tom));
-
-      const datoer = getDateArray(fraDato, tilDato);
+      const datoer = getDateArray(new Date(Date.parse(fraDato)), new Date(Date.parse(tilDato)));
 
       const data = datoer.map((dato) => ({
         dato: format(dato, 'yyyy-MM-dd'),
@@ -88,7 +39,6 @@ const NyMeldeplikt = ({ personident }: { personident: string }) => {
       }));
 
       const postBody = JSON.stringify({ aktivitetPerDag: data });
-      // @ts-ignore
       fetch(`/api/meldeplikt/${personident}`, {
         method: 'POST',
         body: postBody,
@@ -125,28 +75,24 @@ const NyMeldeplikt = ({ personident }: { personident: string }) => {
           <Heading level={'1'} spacing size={'large'}>
             Opprett ny meldeplikt for {personident}
           </Heading>
-          <form onSubmit={(event: React.FormEvent) => postData(event)}>
+          <form onSubmit={(event) => postData(event)}>
             <TextField
-              ref={fradatoRef}
               label={'Fra (åååå-mm-dd)'}
               autoComplete={'off'}
               error={fradatoError}
-              onChange={(event) => {
-                settFraDato(event.target.value);
+              onChange={(e) => {
                 fradatoError && settFraDatoError(undefined);
+                setFraDato(e.target.value);
               }}
-              value={fradato}
             />
             <TextField
-              ref={tildatoRef}
               label={'Til (åååå-mm-dd)'}
               autoComplete={'off'}
               error={tildatoError}
-              onChange={(event) => {
-                settTilDato(event.target.value);
+              onChange={(e) => {
                 tildatoError && settTilDatoError(undefined);
+                setTilDato(e.target.value);
               }}
-              value={tildato}
             />
             <div className={'knapperad'}>
               <Button variant={'primary'} loading={senderData}>
--- src/lib/dates.tsx
+++ src/lib/dates.tsx
@@ -11,3 +11,36 @@ export const tilfeldigDato = (fraAar: number, tilAar: number) => {
   const rand = tilfeldigTall(getUnixTime(stoppAlder), getUnixTime(startAlder));
   return format(fromUnixTime(rand), 'yyyy-MM-dd');
 };
+
+const harVerdi = (input: string) => {
+  if (input === undefined || input === null) {
+    return false;
+  }
+  return !(input === '' || input.trim() === '');
+};
+
+export const validerDato = (dato: string) => {
+  if (!harVerdi(dato)) {
+    return 'Du må legge inn en dato.';
+  }
+  if (dato.toString().length !== 10) {
+    return 'Dato har ugyldig lengde.';
+  }
+  const datoString = dato;
+  const gyldigDato = Date.parse(datoString);
+  if (isNaN(gyldigDato)) {
+    return `${datoString} er en ugyldig dato`;
+  }
+};
+
+export const getDateArray = (start: Date, end: Date) => {
+  const arr = [];
+  const dt = new Date(start);
+
+  while (dt <= end) {
+    arr.push(new Date(dt));
+    dt.setDate(dt.getDate() + 1);
+  }
+
+  return arr;
+};
